b1bc4d1afcf41b394854f98949a37a1ef67c12cd
João Bosco <joaobosconff@JB-MacAirM1.local>
2023-05-04 23:53:28 -0300

    first commit

---
+++ src/main/java/jb/estudo/ferramentas/FerramentasApplication.java
@@ -0,0 +1,13 @@
+package jb.estudo.ferramentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerramentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerramentasApplication.class, args);
+	}
+
+}
