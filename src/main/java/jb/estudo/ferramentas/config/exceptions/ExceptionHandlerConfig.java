a79b8676e31228b823af8e1901d9b84dd8804e80
joaobosconff <joaobosconff03@gmail.com>
2024-04-02 23:14:31 -0300

    refactor

--- src/main/java/jb/estudo/ferramentas/config/exceptions/ExceptionHandlerConfig.java
+++ src/main/java/jb/estudo/ferramentas/config/exceptions/ExceptionHandlerConfig.java
@@ -2,16 +2,12 @@ package jb.estudo.ferramentas.config.exceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jb.estudo.ferramentas.exceptions.BadRequestException;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
